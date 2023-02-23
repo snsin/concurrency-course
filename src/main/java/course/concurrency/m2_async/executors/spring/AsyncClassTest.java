6a4901a9240b423725d4cf8c069290af43fe31c6
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2023-02-23 21:09:34 +0300

    lesson22-hw3: lesson2-2 spring-boot executors learning

--- src/main/java/course/concurrency/m2_async/executors/spring/AsyncClassTest.java
+++ src/main/java/course/concurrency/m2_async/executors/spring/AsyncClassTest.java
@@ -3,10 +3,13 @@ package course.concurrency.m2_async.executors.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
 @Component
+@EnableAsync
 public class AsyncClassTest {
 
     @Autowired
@@ -16,12 +19,22 @@ public class AsyncClassTest {
     @Qualifier("applicationTaskExecutor")
     private ThreadPoolTaskExecutor executor;
 
+    @Autowired
+    private ExternalAsyncClass externalAsyncClass;
+
     public void runAsyncTask() {
-        System.out.println("runAsyncTask: " + Thread.currentThread().getName());
-        internalTask();
+        System.out.println("create async task: " + Thread.currentThread().getName());
+        executor.submit(() -> {
+            var currentThread = Thread.currentThread();
+            System.out.println("runAsyncTask: " + currentThread.getName() + " id: " + currentThread.getId());
+            externalAsyncClass.externalTask();
+            internalTask();
+        });
     }
 
+    @Async
     public void internalTask() {
-        System.out.println("internalTask: " + Thread.currentThread().getName());
+        var currentThread = Thread.currentThread();
+        System.out.println("internalTask: " + currentThread.getName() + " id: " + Thread.currentThread().getId());
     }
 }
