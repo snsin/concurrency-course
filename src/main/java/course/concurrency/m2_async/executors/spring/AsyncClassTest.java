604dcc713794e9f8cc986e29aad0ef0ede41195a
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2023-02-25 19:12:09 +0300

    lesson22-hw3: lesson2-2 make event listener from internal task

--- src/main/java/course/concurrency/m2_async/executors/spring/AsyncClassTest.java
+++ src/main/java/course/concurrency/m2_async/executors/spring/AsyncClassTest.java
@@ -3,6 +3,7 @@ package course.concurrency.m2_async.executors.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -19,20 +20,19 @@ public class AsyncClassTest {
     @Qualifier("applicationTaskExecutor")
     private ThreadPoolTaskExecutor executor;
 
-    @Autowired
-    private ExternalAsyncClass externalAsyncClass;
-
     @Async("applicationTaskExecutor")
     public void runAsyncTask() {
         var currentThread = Thread.currentThread();
         System.out.println("runAsyncTask: " + currentThread.getName() + " id: " + currentThread.getId());
-        externalAsyncClass.externalTask();
-        internalTask();
+        context.publishEvent(new InternalTaskEvent());
     }
 
-    @Async
+    @Async("applicationTaskExecutor")
+    @EventListener(InternalTaskEvent.class)
     public void internalTask() {
         var currentThread = Thread.currentThread();
         System.out.println("internalTask: " + currentThread.getName() + " id: " + Thread.currentThread().getId());
     }
+
+    record InternalTaskEvent() {}
 }
