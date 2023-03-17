177de3be24da09b8e6041efe76c6e433c6e8a865
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2023-03-17 23:53:47 +0300

    lesson36-hw2: implement AuctionPessimistic.java and AuctionOptimistic.java

--- src/main/java/course/concurrency/exams/auction/AuctionOptimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionOptimistic.java
@@ -1,25 +1,32 @@
 package course.concurrency.exams.auction;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class AuctionOptimistic implements Auction {
 
-    private Notifier notifier;
+    private final Notifier notifier;
 
     public AuctionOptimistic(Notifier notifier) {
         this.notifier = notifier;
     }
 
-    private Bid latestBid;
+    private final AtomicReference<Bid> latestBid = new  AtomicReference<>(new Bid(null,
+            null,
+            Long.MIN_VALUE));
 
     public boolean propose(Bid bid) {
-        if (bid.getPrice() > latestBid.getPrice()) {
-            notifier.sendOutdatedMessage(latestBid);
-            latestBid = bid;
-            return true;
-        }
-        return false;
+        Bid outdatedBidCandidate;
+        do {
+            outdatedBidCandidate = latestBid.get();
+            if (outdatedBidCandidate.getPrice() >= bid.getPrice()) {
+                return false;
+            }
+        } while (!latestBid.compareAndSet(outdatedBidCandidate, bid));
+        notifier.sendOutdatedMessage(outdatedBidCandidate);
+        return true;
     }
 
     public Bid getLatestBid() {
-        return latestBid;
+        return latestBid.get();
     }
 }
