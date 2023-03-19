51e36c4f62b957c2e056fa215a847587b6899f18
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2023-03-19 15:58:12 +0300

    lesson37-exam1: access critical section when it really needed

--- src/main/java/course/concurrency/exams/auction/AuctionPessimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionPessimistic.java
@@ -3,24 +3,25 @@ package course.concurrency.exams.auction;
 public class AuctionPessimistic implements Auction {
 
     private final Notifier notifier;
+    private final Object monitor = new Object();
+    private volatile Bid latestBid = new Bid(null, null, Long.MIN_VALUE);
 
     public AuctionPessimistic(Notifier notifier) {
         this.notifier = notifier;
     }
 
-    private volatile Bid latestBid = new Bid(null, null, Long.MIN_VALUE);
-
-    private final Object monitor = new Object();
-
     public boolean propose(Bid bid) {
-        synchronized (monitor) {
-            if (bid.getPrice() > latestBid.getPrice()) {
+        if (bid.getPrice() > latestBid.getPrice()) {
+            synchronized (monitor) {
+                if (bid.getPrice() <= latestBid.getPrice()) {
+                    return false;
+                }
                 notifier.sendOutdatedMessage(latestBid);
                 latestBid = bid;
                 return true;
             }
-            return false;
         }
+        return false;
     }
 
     public Bid getLatestBid() {
