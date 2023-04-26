4391b5873066a44453dcbf221186d3aae5883c53
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2023-04-26 23:21:39 +0300

    lesson42-hw1: fix answer and code - FjpExample.java

--- src/main/java/course/concurrency/m4_fj/lesson42/FjpExample.java
+++ src/main/java/course/concurrency/m4_fj/lesson42/FjpExample.java
@@ -1,14 +1,14 @@
 package course.concurrency.m4_fj.lesson42;
 
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 public class FjpExample {
     public static void main(String[] args) {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        ForkJoinTask<?> task = forkJoinPool.submit(() -> System.out.println(
+        forkJoinPool.submit(() -> System.out.println(
                 IntStream.range(0, 10000000).average().getAsDouble()));
-        task.join();
+        forkJoinPool.awaitQuiescence(1, TimeUnit.MINUTES);
     }
 }
