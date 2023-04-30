3d9ce3a092cbf76a736485b64dd156e726ff14c3
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2023-04-30 18:07:39 +0300

    lesson54-exam1: implement tests for old MountTableRefresherService version

--- src/test/java/course/concurrency/exams/refactoring/MountTableRefresherServiceTests.java
+++ src/test/java/course/concurrency/exams/refactoring/MountTableRefresherServiceTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.mockito.internal.stubbing.answers.AnswersWithDelay;
 
 import java.util.List;
 
@@ -17,6 +18,7 @@ public class MountTableRefresherServiceTests {
 
     private Others.RouterStore routerStore;
     private Others.MountTableManager manager;
+    @SuppressWarnings("rawtypes")
     private Others.LoadingCache routerClientsCache;
 
     @BeforeEach
@@ -47,7 +49,7 @@ public class MountTableRefresherServiceTests {
         when(manager.refresh()).thenReturn(true);
 
         List<Others.RouterState> states = addresses.stream()
-                .map(a -> new Others.RouterState(a)).collect(toList());
+                .map(Others.RouterState::new).collect(toList());
         when(routerStore.getCachedRecords()).thenReturn(states);
         // smth more
 
@@ -55,6 +57,7 @@ public class MountTableRefresherServiceTests {
         mockedService.refresh();
 
         // then
+        verify(mockedService, times(0)).log("Not all router admins updated their cache");
         verify(mockedService).log("Mount table entries cache refresh successCount=4,failureCount=0");
         verify(routerClientsCache, never()).invalidate(anyString());
     }
@@ -63,24 +66,114 @@ public class MountTableRefresherServiceTests {
     @DisplayName("All tasks failed")
     public void noSuccessfulTasks() {
 
+        // given
+        MountTableRefresherService mockedService = Mockito.spy(service);
+        List<String> addresses = List.of("999", "local3", "765", "local7");
+
+        when(manager.refresh()).thenReturn(false);
+
+        List<Others.RouterState> states = addresses.stream()
+                .map(Others.RouterState::new).collect(toList());
+        when(routerStore.getCachedRecords()).thenReturn(states);
+        // smth more
+
+        // when
+        mockedService.refresh();
+
+        // then
+        verify(mockedService, times(0)).log("Not all router admins updated their cache");
+        verify(mockedService).log("Mount table entries cache refresh successCount=0,failureCount=4");
+        List<String> adminAddresses = states.stream()
+                .map(Others.RouterState::getAdminAddress)
+                .collect(toList());
+        verify(routerClientsCache, times(4)).invalidate(argThat(adminAddresses::contains));
     }
 
     @Test
     @DisplayName("Some tasks failed")
     public void halfSuccessedTasks() {
+        // given
+        MountTableRefresherService mockedService = Mockito.spy(service);
+        List<String> addresses = List.of("333", "localA", "135", "localC");
 
+        when(manager.refresh()).thenReturn(false)
+                .thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(false);
+
+        List<Others.RouterState> states = addresses.stream()
+                .map(Others.RouterState::new).collect(toList());
+        when(routerStore.getCachedRecords()).thenReturn(states);
+        // smth more
+
+        // when
+        mockedService.refresh();
+
+        // then
+        verify(mockedService, times(0)).log("Not all router admins updated their cache");
+        verify(mockedService).log("Mount table entries cache refresh successCount=2,failureCount=2");
+        List<String> adminAddresses = states.stream()
+                .map(Others.RouterState::getAdminAddress)
+                .collect(toList());
+        verify(routerClientsCache, times(2)).invalidate(argThat(adminAddresses::contains));
     }
 
     @Test
     @DisplayName("One task completed with exception")
     public void exceptionInOneTask() {
+        // given
+        MountTableRefresherService mockedService = Mockito.spy(service);
+        List<String> addresses = List.of("7788", "localR8", "4365", "localL0");
 
+        when(manager.refresh()).thenReturn(true)
+                .thenThrow(RuntimeException.class)
+                .thenReturn(true)
+                .thenReturn(true);
+
+        List<Others.RouterState> states = addresses.stream()
+                .map(Others.RouterState::new).collect(toList());
+        when(routerStore.getCachedRecords()).thenReturn(states);
+        // smth more
+
+        // when
+        mockedService.refresh();
+
+        // then
+        verify(mockedService, times(0)).log("Not all router admins updated their cache");
+        verify(mockedService).log("Mount table entries cache refresh successCount=3,failureCount=1");
+        List<String> adminAddresses = states.stream()
+                .map(Others.RouterState::getAdminAddress)
+                .collect(toList());
+        verify(routerClientsCache).invalidate(argThat(adminAddresses::contains));
     }
 
     @Test
     @DisplayName("One task exceeds timeout")
     public void oneTaskExceedTimeout() {
+        // given
+        MountTableRefresherService mockedService = Mockito.spy(service);
+        List<String> addresses = List.of("addr", "localD7", "9987", "local90");
 
+        when(manager.refresh()).thenReturn(true)
+                .thenAnswer(new AnswersWithDelay(1500L, invocation -> true))
+                .thenReturn(true)
+                .thenReturn(true);
+
+        List<Others.RouterState> states = addresses.stream()
+                .map(Others.RouterState::new).collect(toList());
+        when(routerStore.getCachedRecords()).thenReturn(states);
+        // smth more
+
+        // when
+        mockedService.refresh();
+
+        // then
+        verify(mockedService).log("Not all router admins updated their cache");
+        verify(mockedService).log("Mount table entries cache refresh successCount=3,failureCount=1");
+        List<String> adminAddresses = states.stream()
+                .map(Others.RouterState::getAdminAddress)
+                .collect(toList());
+        verify(routerClientsCache).invalidate(argThat(adminAddresses::contains));
     }
 
 }
